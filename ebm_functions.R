74e337a797bf5dd0ab0778afb5dd5ebf7bbaeab6
Kira Rehfeld <krehfeld@iup.uni-heidelberg.de>
2019-03-22 16:41:03 +0100

    added temperature-dependent albedo

--- ebm_functions.R
+++ ebm_functions.R
@@ -76,6 +76,43 @@ rout<-list(yout=yout,forcing=forcing.ts)
 return(rout)
 }
 
+
+ebm.0d.icealb<-function(time.out,Flux,t.initial=288,interp="constant",plot=FALSE,parms=list(f=0.7,h=70,S=1365,eps=0.6,alphamax=0.3,kappa=1)){
+  #  attach(parms)
+  # effective heat capacity of a 1 square meter area of the earth
+  # C = f*rho*c_water*h
+  # f fraction of Earth covered by water (.7)
+  # rho density of sea water 1025kg/m^3
+  # c specific heat of water (4186 J/kgK)
+  # h depth of the mixed layer (70m)
+  Q_0<-1/4*parms$S
+  rho<-1025
+  c<-4186
+  C<-parms$f*rho*c*parms$h
+  
+  if (interp=="constant") Frc<-approxfun(x=Flux[,1],y=Flux[,2],method = "constant",rule=2)
+  if (interp=="linear") Frc<-approxfun(x=Flux[,1],y=Flux[,2],method = "linear",rule=2)
+  
+  #params<-c(C=C,alpha=parms$alpha,eps=parms$eps,Q_0=Q_0)
+  ebm2<-function(t,y,parms){
+    Q<-Frc(t)+Q_0
+    Tc=273
+    c2=0.7
+    c1=0.15
+#    alphat<-parms$alphamax+b(1-tanh(parms$kappa(y-272)))/2
+       alphat<-c1+c2*(1-tanh(parms$kappa(y-Tc)))/2
+    sigma<-5.67*10^(-8)
+    list(1/C*((1-alphat)*Q-sigma*parms$eps*y^4))
+  }
+  
+  yout<-ode(y=t.initial,func=ebm2,times=time.out,parms=parms)
+  if (plot) {plot(yout[,1],yout[,2])
+    grid()
+  }
+  forcing.ts<-sapply(time.out,Frc)
+  rout<-list(yout=yout,forcing=forcing.ts)
+  return(rout)
+}
 # yout<-ebm.0d(seq(0,60*60*24*365*100,by=60*60*24),Flux=cbind(0,10),plot=TRUE)
 ## Utility function changes
 
@@ -238,3 +275,120 @@ plotxy_inset<-function(tsx1,tsy2,col1="indianred",col2="darkblue",timunit="year 
   mtext(side = 4,col=col2,at=yax,text = yax,las=1,line=0.8)
   mtext(side=4,col=col2,ax4unit,font=2,line=2.5,at=mean(range(tsy2)),las=0)
 }
+
+
+
+#' Get the 0d Energy Balance including a temperature-dependent albedo
+#'
+#' @param Flux 2D vector with time and external radiative flux in W/m^2
+#' @param inttime integration time
+#' @param tunit time unit (default "years")
+#' @param equilyears number of equilibration years that will be removed from inttime for the initial model equilibration
+#' @param plot logical TRUE/FALSE
+#' @param interp interpolation scheme for the radiative flux ("constant", "linear")
+#' @param parms additional parameters for the EBM
+#'
+#' @return list 
+#' @export
+#'
+#' @examples
+#' inttime<-seq(0,100)
+#' Flux<-rep(0,length(inttime))
+#' getebm.0d.tempdep(Flux,inttime,plot=TRUE)
+getebm.0d.tempdep<-function(Flux,inttime,tunit="years",equilyears=0,plot=FALSE,interp="constant",parms=list(f=0.7,h=70,S=1365,eps=0.6,alphamax=0.3,kappa=1,Tinit=288)){
+  if (tunit=="years") {
+    Flux[,1]<-Flux[,1]*60*60*24*365
+    inttime<-inttime*60*60*24*365} else {tunit<-"seconds"}
+  #Frc<-approxfun(x=Flux[,1],y=Flux[,2],method = "constant",rule=2)
+  #inttime<-Flux[,1]
+  rout<-ebm.0d.icealb(inttime,Flux=Flux,parms=parms,interp=interp,plot=FALSE)
+  yout<-rout$yout
+  yout.cut<-yout[-c(1:equilyears),]
+  forcing.ts<-rout$forcing[-c(1:equilyears)]
+  tsout<-zoo(yout.cut[,2],order.by=yout.cut[,1]/(60*60*24*365))
+  c1<-0.15 #min albedo
+  c2<-0.7 # max albedo = c1+c2
+  albedo=zoo(c1+c2*(1-tanh(parms$kappa*(coredata(tsout)-273)))/2,order.by=index(tsout))
+  #GHG
+  T06<-1.9*10^{-15} # [K^{-6}]sellers 1969, ghil 1976, T_0^{-6]}
+  m<-0.4
+  gofT<-1-m*tanh((coredata(tsout)^6*T06))
+  sigma<-5.67*10^(-8)
+  
+  Rout<-zoo(sigma*gofT*coredata(tsout)^4,order.by=index(tsout))
+  Rin<-((1-coredata(albedo))*(parms$S/4)+coredata(forcing.ts))
+  
+  if (plot) {
+    par(mfcol=c(3,1),mar=c(4,4,0,1))
+    plot(tsout,axes=FALSE,xlab="",ylab="")
+    axis(2);mtext(side=2,line=2,"GMST [K]")
+    abline(h=273,col="indianred")
+    plot(albedo,axes=FALSE,xlab="",ylab="")
+    abline(h=c(c1,c1+c2),lty=2,col="indianred")
+    axis(2);mtext(side=2,line=2,"albedo [0-1]")
+    plot(Rout,xlab="",ylab="")
+    mtext(side=2,line=2,"R_out [W/m^2]")
+  }
+  
+  rlist<-list(tsout=tsout,forcing.ts=forcing.ts,misc=list(tunit=tunit,yout=yout,equilyears=equilyears,parms=parms),albedo=albedo,Rout=Rout,Rin=Rin)
+  return(rlist)
+}
+
+
+#' Energy balance model with temperature-dependent albedo (internal function)
+#'
+#' @param time.out vector of integration time
+#' @param Flux vector with radiative forcing 
+#' @param interp interpolation type for Flux ("constant","linear")
+#' @param plot logical
+#' @param parms additional parameters to pass to EBM
+#'
+#' @return list
+#' @export
+#'
+#' @examples
+ebm.0d.icealb<-function(time.out,Flux,interp="constant",plot=FALSE,parms=list(f=0.7,h=70,S=1365,eps=0.6,alphamax=0.3,kappa=1,Tinit=288)){
+  #  attach(parms)
+  # effective heat capacity of a 1 square meter area of the earth
+  # C = f*rho*c_water*h
+  # f fraction of Earth covered by water (.7)
+  # rho density of sea water 1025kg/m^3
+  # c specific heat of water (4186 J/kgK)
+  # h depth of the mixed layer (70m)
+  Q_0<-1/4*parms$S
+  rho<-1025
+  c<-4186
+  C<-parms$f*rho*c*parms$h
+  
+  if (interp=="constant") Frc<-approxfun(x=Flux[,1],y=Flux[,2],method = "constant",rule=2)
+  if (interp=="linear") Frc<-approxfun(x=Flux[,1],y=Flux[,2],method = "linear",rule=2)
+  
+  #params<-c(C=C,alpha=parms$alpha,eps=parms$eps,Q_0=Q_0)
+  ebm2<-function(t,y,parms){
+    Q<-Frc(t)+Q_0
+    Tc=273
+    c2=0.7
+    c1=0.15
+    T06<-1.9*10^{-15} # [K^{-6}]sellers 1969, ghil 1976, T_0^{-6]}
+    m<-0.4
+    gofT<-1-m*tanh((y^6*T06))
+    #    alphat<-parms$alphamax+b(1-tanh(parms$kappa(y-272)))/2
+    alphat<-c1+c2*(1-tanh(parms$kappa*(y-Tc)))/2
+    sigma<-5.67*10^(-8)
+    #    eb<-1/C*((1-alphat)*Q)-sigma*parms$eps*y^4 # use gofT instead of parms$eps
+    #    list(eb)
+    list(1/C*((1-alphat)*Q-sigma*gofT*y^4))
+  }
+  #print(parms$Tinit)
+  yout<-ode(y=parms$Tinit,func=ebm2,times=time.out,parms=parms)
+  head(yout)
+  if (plot) {plot(yout[,1],yout[,2])
+    grid()
+  }
+  
+  
+  forcing.ts<-sapply(time.out,Frc)
+  rout<-list(yout=yout,forcing=forcing.ts)
+  return(rout)
+}
+
